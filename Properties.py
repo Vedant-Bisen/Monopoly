5c710083ee82bbde182dddb0c617c3049a57c459
Vedant Bisen <bisenvedant@gmail.com>
2022-05-24 13:45:29 +0530

    .

---
+++ Properties.py
@@ -0,0 +1,10 @@
+class Properties(object):
+
+    def __init__(self,Name,Price,Position,Houses,Owner):
+        self.Name = Name
+        self.Price = Price
+        self.Position = Position
+        self.Houses = 0
+        self.Owner = False
+
+    def 
