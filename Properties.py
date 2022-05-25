83d244256bfeacb2ff5ea503272eb8ffb8d04354
Vedant Bisen <bisenvedant@gmail.com>
2022-05-25 20:57:24 +0530

    .

--- Properties.py
+++ Properties.py
@@ -1,21 +1,18 @@
 
-from Monopoly.Players import Players
+from Players import *
 
 
 class Properties(object):
 
-    def __init__(self,Name,Price,Position,Houses,Owner):
+    def __init__(self,Name,Price,Position):
         self.Name = Name
-        self.Price = Price
+        self.Price = int(Price)
         self.Position = Position
         self.Houses = 0
         self.Owner = ""
     
     def Call_Name(self):
         return self.Name
-
-    def Call_Price(self):
-        return self.Price
 
     def Call_Position(self):
         return self.Position
