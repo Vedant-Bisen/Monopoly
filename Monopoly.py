c176a92aa9f234afc4445bc3eaecd9999a4ed427
Vedant Bisen <bisenvedant@gmail.com>
2022-06-02 11:10:30 +0530

    .

--- Monopoly.py
+++ Monopoly.py
@@ -1,10 +1,20 @@
 from Players import *
 from Properties import *
 
+def move(Player,Dice):
+    
 
-vedant = Players("vedant",1000,0)
-OLd_chap = Properties("OldChap",500,0)
-print(Players.Call_Money(vedant))
-print(Players.Call_Props(vedant))
-Players.Buy(vedant)
-print(Players.Call_Props(vedant))
+def buy(Player,Property):
+    if Players.Call_Money(Player)>Properties.Call_Price(Property):
+        Player.Money -= Property.Price
+        Property.Owner = str(Player)
+        Player.Props.append(Properties.Call_Name(Property))
+        Property.Owner = Players.Call_Name(Player)
+        print(Properties.Call_Name(Property),"bought by",Players.Call_Name(Player))
+    else:
+        print(Players.Call_Name(Player),"does not have enough money to buy ",Properties.Call_Name(Property))
+
+vedant = Players("Vedant",1000,0)
+Old_Kent = Properties("OldKent",2000,0)
+
+#buy(vedant,Old_Kent)
