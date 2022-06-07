dd8a23d1d51cf3b6e355cf47ad742850eb8993ee
Vedant Bisen <bisenvedant@gmail.com>
2022-06-07 13:12:47 +0530

    .

--- Monopoly.py
+++ Monopoly.py
@@ -2,20 +2,25 @@ from Players import *
 from Properties import *
 from Board import *
 
-def Play():
-    PlayOrder = []
+PlayOrder = []
+PlOrder = 0
+def Start():
     i = int(input("How many players wish to play: "))
     for i in range(0,i):
         Name = input("Enter the Name of player: ")
         Name = Players(Name,5000,0)
         PlayOrder.append(Name)
         print(PlayOrder)
+
+def Play():
+    print("Order of moving is: ")
+    for i in range(0,len(PlayOrder)):
+        print(PlayOrder[i].Call_Name())
     
 
 
 
 
 
-
-
+Start()
 Play()
